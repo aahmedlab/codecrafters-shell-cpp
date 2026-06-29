557446ffff07890ff7200dbedaf8708ba42fb57a
Abdullah Ahmed <251920537+aahmedlab@users.noreply.github.com>
2026-06-28 23:39:49 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
   cout << std::unitbuf;
   cerr << std::unitbuf;
+
+  string line;
   string command;
+  string data;
   while (true)
   {
     cout << "$ ";
     // Safely reads the entire line at once
-    if (!getline(cin, command)) {
+    if (!getline(cin, line)) {
       break;
     }
 
-    if (command.empty()) {
+    if (line.empty()) {
+      continue;
+    }
+    istringstream iss(line);
+    iss >> command;
+    if (command == "exit") break;
+    if (command == "echo")
+    {
+      getline(iss, data);
+      if (!data.empty() && data[0] == ' ')
+      {
+        data = data.substr(1);
+      }
+      cout << data << "\n";
       continue;
     }
 
-    if (command == "exit") break;
-
-    cout << command << ": command not found\n";
+    cout << line << ": command not found\n";
   }
   return 0;
 }
